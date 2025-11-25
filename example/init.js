96831e7baec0c8d3b99904494783cffc86ac83b1
gamk3 <58887107+gamk3@users.noreply.github.com>
2025-11-25 09:35:02 +0000

    Updated example

--- example/init.js
+++ example/init.js
@@ -1,9 +1,9 @@
 // Example initializer for markdown-it in the webview.
-// This function will be called with the `markdown-it` instance as the only argument.
+// This function will be called with the `markdown-it` instance and the value true as the only two arguments.
 
 // This is where you can add plugins etc...
 // Because this is just some file in your project, you can reuse this cofinguration
 // for your actual code that renders markdown!
-initMarkdownIt = function(md) {
+export function initMarkdownIt(md, isExtension) {
   md.use(markdownitContainer, "info");
 };
